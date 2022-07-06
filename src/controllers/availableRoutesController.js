a15761c5e0bf39dabaed15519ea0df32c831a7e0
Vinayak Surshetty <vnksurshetty@gmail.com>
2022-07-06 22:08:33 +0530

    cors addedin routes controller

--- src/controllers/availableRoutesController.js
+++ src/controllers/availableRoutesController.js
@@ -1,7 +1,11 @@
 const express = require("express");
 var RouteDetail = require("../models/routeModel");
 
-module.exports.getAvailableRoutes = (req, res)=>{
+module.exports.getAvailableRoutes = (req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
   RouteDetail.find({}, function(err, result) {
     if (err) {
       console.log(err);
@@ -12,7 +16,11 @@ module.exports.getAvailableRoutes = (req, res)=>{
 }
 
 
-module.exports.addAvailableRoutes = async (req, res)=>{
+module.exports.addAvailableRoutes = async (req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
   var myobj = [     
     { 'r_id': 12, 'destination': "Mumbai", 'source': "Ghaziabad"},  
     { 'r_id': 13, 'destination': "Mumbai", 'source': "CA"},  
