ea9f31cc482087a1bc009a39c30616149a3b399d
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-07-12 16:27:55 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -9,8 +9,17 @@ const indexRoutes = require('./routes/indexRoutes');
 const journeyDetailsRouter = require('./routes/journeyDetailsRouter');
 const app = express();
 
-app.use(cors())
+
 app.use(express.json());
+
+// Activating CORS
+// app.use(cors({
+//   'allowedHeaders': ['sessionId', 'Content-Type'],
+//   'exposedHeaders': ['sessionId'],
+//   'origin': '*',
+//   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   'preflightContinue': false
+// }));
 
 
 // Database connection
@@ -30,9 +39,27 @@ db.once("open", function () {
 
  
 // Activating CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
 
